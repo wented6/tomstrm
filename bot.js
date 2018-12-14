100a30517e5935db972d672a1f1d56b7e1f5dcf1
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-14 21:21:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,59 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'),
+bot = new Discord.Client({sisableEveryone: true})
+console.log("Kiiira,");
 const client = new Discord.Client();
-const developers = ['462503125048360960'];
 
 
 
 
+ const devs = ['522439536706322462','522439536706322462','522439536706322462'];
+const adminprefix = "!";
 client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!developers.includes(message.author.id)) return;
+    var argresult = message.content.split(`).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(  ${argresult} ply .. :white_check_mark:)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(  ${argresult} wt .. :white_check_mark:)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(  ${argresult} ls .. :white_check_mark:)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+      message.channel.sendMessage(  ${argresult} st .. :white_check_mark:)
+    }
+  if (message.content.startsWith(adminprefix + 'us')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(us ..${argresult} )
+  } else
+  if (message.content.startsWith(adminprefix + 'av')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(avatar ... :** `);
+  }
+ });
 
-if (message.content.startsWith('#wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.send(`${argresult} \` `)
-} else 
-if (message.content.startsWith('#ls')) {
-client.user.setActivity(argresult , {type:'LISTENING'});
-    message.channel.send(` ${argresult} \` `)
-} else 
-if (message.content.startsWith('#st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/meercy");
-   message.channel.send(`${argresult} \` `)
-}
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
